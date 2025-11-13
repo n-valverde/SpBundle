ce57fbde6de7c3cf32d12b9f3fe21a1343676a27
Nicolas Valverde <64469669+n-valverde@users.noreply.github.com>
2025-11-13 08:30:20 +0100

    Symfony 5 compatibility

--- src/LightSaml/SpBundle/DependencyInjection/Configuration.php
+++ src/LightSaml/SpBundle/DependencyInjection/Configuration.php
@@ -25,8 +25,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $root = $treeBuilder->root('light_saml_sp');
+        if (method_exists(TreeBuilder::class, 'getRootNode')) {
+            $treeBuilder = new TreeBuilder('light_saml_sp');
+            $root = $treeBuilder->getRootNode();
+        } else {
+            $treeBuilder = new TreeBuilder();
+            $root = $treeBuilder->root('light_saml_sp');
+        }
 
         $root
             ->children()
